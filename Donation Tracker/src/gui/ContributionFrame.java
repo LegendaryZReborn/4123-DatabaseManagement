bbd489d895d356d1a58bebbe23d7f0ecd9ed1100
unknown <BO103LabStudent@BO103-LAB-0004.labs.msu>
2017-11-13 11:56:04 -0600

    fixed_date

--- Donation Tracker/src/gui/ContributionFrame.java
+++ Donation Tracker/src/gui/ContributionFrame.java
@@ -461,7 +461,6 @@ public class ContributionFrame extends javax.swing.JFrame {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             java.util.Date date = sdf.parse(sDate);
             java.sql.Date sqlDate = new java.sql.Date(date.getTime());
-            String fund = fundComboBox.getSelectedItem().toString();
             Contribution contribution =   new Contribution(Double.parseDouble
                 (amountTextField.getText()),sqlDate,noteTextPane.getText(),
                     typeComboBox.getSelectedItem().toString(),fundComboBox.
