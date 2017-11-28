b45cd74824f4d3b202764689dae332c4ba8e062a
ChrisDuhan <chris.m.duhan@gmail.com>
2017-11-28 11:05:26 -0600

    A fix for TAR 3

--- Donation Tracker/src/gui/ContributionFrame.java
+++ Donation Tracker/src/gui/ContributionFrame.java
@@ -391,10 +391,7 @@ public class ContributionFrame extends javax.swing.JFrame {
 
         int k = Integer.parseInt(model.getValueAt(a,2).toString());
         envComboBox.setSelectedIndex(k);
-        DecimalFormat decForm = new DecimalFormat("0.00");
-        String formattedText = decForm.format(model.getValueAt(a, 3));
-        //amountTextField.setText(model.getValueAt(a, 3).toString());
-        amountTextField.setText(formattedText);
+        amountTextField.setText(model.getValueAt(a, 3).toString());
 
         dateTextField.setText(model.getValueAt(a, 1).toString());
         //fund combo box
